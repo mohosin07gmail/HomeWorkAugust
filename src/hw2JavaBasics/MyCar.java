3250f82e11f461c8cc6edc6abf466477b2139196
mohosin07gmail <mohosin07@gmail.com>
2023-09-01 15:58:01 -0400

    second home work

---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,11 @@
+package hw2JavaBasics;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Md Mosharrof Hussain" + "\nI love to drive" + "\nMy car info is below");
+		// color, model, year, wheel drive
+		System.out.println("Brand: Nissan \tColor: Metalgrey \tModel: Pathfinder \tYear: 2016 \tWheel Drive: 4wd");
+	}
+
+}

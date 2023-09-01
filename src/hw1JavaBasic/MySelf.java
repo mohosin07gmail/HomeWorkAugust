a6d76f24ec5421e9f5cdb08d4ce02440df13aa99
mohosin07gmail <mohosin07@gmail.com>
2023-09-01 15:20:13 -0400

    first homework

---
+++ src/hw1JavaBasic/MySelf.java
@@ -0,0 +1,15 @@
+package hw1JavaBasic;
+/* project name starts with upper case
+ * package name starts with lower case
+ * class name starts with upper case 
+ */
+
+public class MySelf {
+
+	// main method is the method from where all the coding gets executed.
+	public static void main(String[] args) {
+		System.out.println("I am Md Mosharrof Hussain");
+		System.out.println("56 Southgate Road, Buffalo, NY");
+	}
+
+}

ae8f927447051d1570a9da65b3c311f836e3d2f3
mohosin07gmail <mohosin07@gmail.com>
2023-09-02 01:41:54 -0400

    variable initialized

---
+++ src/hw4JavaVariablesInitialized/AboutMeTest.java
@@ -0,0 +1,12 @@
+package hw4JavaVariablesInitialized;
+
+public class AboutMeTest {
+
+	public static void main(String[] args) {
+		// object is instantiated
+		AboutMe objAboutMe = new AboutMe();
+
+		// method is implemented
+		objAboutMe.aboutMe();
+	}
+}

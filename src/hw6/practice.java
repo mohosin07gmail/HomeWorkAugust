cdcb3ba0f6822ef7bcc5acf2502cf1c1a58f1be9
mohosin07gmail <mohosin07@gmail.com>
2023-09-14 14:00:44 -0400

    hw6IfElseCondition01

---
+++ src/hw6/practice.java
@@ -0,0 +1,11 @@
+package hw6;
+
+public class practice {
+	
+	public static void main(String[] args) {
+		String name2 = "100welcome";
+		String name3 = "3.15";
+		System.out.println(name3);
+	}
+
+}

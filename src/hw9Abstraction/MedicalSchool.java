da528da436f802267cc27bb9ab795e599584422c
mohosin07gmail <mohosin07@gmail.com>
2023-09-25 13:07:24 -0400

    abstract method

---
+++ src/hw9Abstraction/MedicalSchool.java
@@ -0,0 +1,16 @@
+package hw9Abstraction;
+
+public abstract class MedicalSchool {
+	
+	public abstract void anatomyLab ();
+	
+	public void biochemistryLab() {
+		System.out.println("this is non abstract or implemented method");		
+	}
+	
+	
+	
+	
+	
+
+}
